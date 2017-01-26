e6649bb30dc1177c72e192d701c118328fb61a98
RudaevDima <RudaevDima@gmail.com>
2017-01-26 08:16:19 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,66 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import operator
+import re
 
+infile_path = "/home/gonzzz/Downloads/project2.txt"
+outfile_path = "dict.txt"
 
-with open('/home/gonzzz/Downloads/project2.txt', 'r') as myfile:
-    data=myfile.read().decode('cp1255').encode('utf-8',errors='replace')
+with open(infile_path, 'r') as text_file:
+    data=text_file.read().decode('cp1255').encode('utf-8', errors='replace')
     #print data
 
+    data = data.replace(r'-', r'.')
+    data = data.replace(r'+', r'.')
+    data = data.replace(r'%', r'.')
+    data = data.replace(r'/', r'.')
+    data = data.replace(r'*', r'.')
+    data = data.replace('\t', r'.')
+    data = data.replace('\n', r'.')
+    data = data.replace('\r', r'.')
+    data = data.replace('\v', r'.')
+    data = data.replace(r"''", r'.')
+    data = data.replace(" ", '.')
+    data = data.replace(",", '.')
+    data = data.replace(")", '.')
+    data = data.replace("(", '.')
+    data = data.replace("0", '.')
+    data = data.replace("1", '.')
+    data = data.replace("2", '.')
+    data = data.replace("3", '.')
+    data = data.replace("4", '.')
+    data = data.replace("5", '.')
+    data = data.replace("6", '.')
+    data = data.replace("7", '.')
+    data = data.replace("8", '.')
+    data = data.replace("9", '.')
 
-    data2 = data.replace(r'-', r'.')
-    data2 = data2.replace(r'+', r'.')
-    data2 = data2.replace(r'%', r'.')
-    data2 = data2.replace(r'/', r'.')
-    data2 = data2.replace(r'*', r'.')
-    data2 = data2.replace(r'\t', r'.')
-    data2 = data2.replace(r'\n', r'.')
-    data2 = data2.replace(r'\r', r'.')
-    data2 = data2.replace(r'\v', r'.')
-    data2 = data2.replace(r"''", r'.')
-    data2 = data2.replace(" ",'.')
-    data2 = data2.replace(",",'.')
-    data2 = data2.replace(")", '.')
-    data2 = data2.replace("(", '.')
-    data2 = data2.replace("0", '.')
-    data2 = data2.replace("1", '.')
-    data2 = data2.replace("2", '.')
-    data2 = data2.replace("3", '.')
-    data2 = data2.replace("4", '.')
-    data2 = data2.replace("5", '.')
-    data2 = data2.replace("6", '.')
-    data2 = data2.replace("7", '.')
-    data2 = data2.replace("8", '.')
-    data2 = data2.replace("9", '.')
+    print "--------------------------------- data after striping  ------------------------------------"
+    print data
 
-    print "-------------------------------------------------------------------------------------"
-    print data2
-
-    data3=data2.split(".")
-    print "-------------------------------------------------------------------------------------"
-    print "-------------------------------------------------------------------------------------"
-    print data3
+    data=data.split(".")
+    print "--------------------------------- data after spliting  -------------------------------------"
+    print data
 
 aDict = dict()
 
 
-for word in data3:
+for word in data:
 
-    if word != '':
+    if word != '' and  not re.search('[a-zA-Z]', word):
 
-        word2 = word.decode('utf-8').encode('cp1255',errors='replace')
-        if word2 not in aDict :
-            aDict[word2] = 1
+        word = word.decode('utf-8').encode('cp1255', errors='replace')
+
+        if word not in aDict :
+            aDict[word] = 1
         else:
-            aDict[word2] = aDict[word2] + 1
+            aDict[word] = aDict[word] + 1
     else:
         #print "skipping word:" + word
         pass
 
-with open("dict.txt", "a") as outfile:
+with open(outfile_path, "a") as outfile:
 
-    for pair in  aDict:
-        print str(aDict[pair]) + " : "+ pair.decode('cp1255').encode('utf-8',errors='replace')
-        outfile.write(str(aDict[pair]) + " : "+ pair.decode('cp1255').encode('utf-8',errors='replace') + "\n")
+    sortedList = sorted(aDict.items(), key=operator.itemgetter(1))
 
+    for t in sortedList:
+        print t[0].decode('cp1255').encode('utf-8',errors='replace') + ":" + str(t[1])
+        outfile.write(t[0].decode('cp1255').encode('utf-8',errors='replace') + ":" + str(t[1]) )
+        outfile.write("\n")
 
-print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+print "------------------------- Done , results written to " + outfile_path + "---------------------------"
